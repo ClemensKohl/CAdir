07d5c39e337fd26d80eff963079c2a1756bbd688
ClemensKohl <kohl.clemens@gmail.com>
2024-01-16 09:41:35 +0100

    upd

---
+++ R/clustering.R
@@ -0,0 +1,11 @@
+
+# ** Plan: **
+# There is one main function that performs clustering by direction.
+# Depending on the choice of a paremter we branch into Salpha clustering or
+# regular splitmerging (e.g. setting cutoff = auto or similar)
+# 
+
+dirclust <- function() {
+
+}
+
