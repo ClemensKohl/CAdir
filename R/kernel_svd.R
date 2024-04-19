f07f4bfc85c6d5de47a22282fa12d5d6feddb0d2
Clemens Kohl <kohl.clemens@gmail.com>
2024-04-19 15:02:23 +0200

    bad fix for kernel ca

--- R/kernel_svd.R
+++ R/kernel_svd.R
@@ -37,7 +37,7 @@
 #         return(ret)
 #     }
 # )
-
+loadNamespace("kernlab")
 setClass("rbfkernel",prototype=structure(.Data=function(){},kpar=list()),contains=c("kernel"))
 rbfdot <- function(sigma = 1) {
     rval <- function(x, y = NULL) {
@@ -92,32 +92,47 @@ kernel_ca <- function(obj,
                                 residuals = residuals,
                                 clip = clip,
                                 cutoff = cutoff)
-    s <- t(res$S)
+    # s <- t(res$S)
 
     # FIXME: The function centers the data, we dont want that.
-    kpca <- kernlab::kpca(x = s,
-                          kernel = kernel,
-                          features = dims,
-                          kpar = kpar)
+    # kpca <- kernlab::kpca(x = s,
+    #                       kernel = kernel,
+    #                       features = dims,
+    #                       kpar = kpar)
 
-    s <- as.matrix(res$S)
-    kernel <- do.call(kernel, kpar)
-    km <- kernlab::kernelMatrix(kernel = kernel, x = s)
+    s <- as.matrix(t(res$S))
+    kern <- do.call(kernel, kpar)
+    km <- kernlab::kernelMatrix(kernel = kern, x = s)
+
+    ## center kernel matrix
+    # kc <- t(t(km - colSums(km) / m) - rowSums(km) / m) + sum(km) / m^2
+
+    ## compute eigenvectors
+    m <- nrow(s)
+    kpca <- eigen(km / m, symmetric = TRUE)
+
+    v <- t(t(kpca$vectors[, 1:dims]) / sqrt(kpca$values[1:dims]))
+    e <- kpca$values[1:dims]
+    d <- sqrt(e) # D <- sqrt(E)*(nrow(S) - 1)
+    names(e) <- paste("Comp.", 1:dims, sep = "")
+    prin_coords_cols <- km %*% v
+
+    # NOTE: We have to provide a matrix U, so we make a dummy vector.
+    u <- matrix(NA, nrow = ncol(s), ncol = dims)
 
     # eigenvalues in a decreasing order
-    svd_res <- irlba::irlba(A = km, nv = dims, smallest = FALSE)
-    svd_res <- svd_res[1:3]
-    names(svd_res)[1:3] <- c("D", "U", "V")
-    svd_res$D <- as.vector(svd_res$D)
+    # svd_res <- irlba::irlba(A = km, nv = dims, smallest = FALSE)
+    # svd_res <- svd_res[1:3]
+    # names(svd_res)[1:3] <- c("D", "U", "V")
+    # svd_res$D <- as.vector(svd_res$D)
 
     # S = UDV^T
     # cxg = [cxd][dxd][dxg]
 
-    v <- kpca@pcv
-    e <- kpca@eig
-    d <- sqrt(e) # D <- sqrt(E)*(nrow(S) - 1)
-    u <- t(diag(1 / d) %*% t(v) %*% kpca@xmatrix) # TODO: Check if correct
-
+    # v <- kpca@pcv
+    # e <- kpca@eig
+    # d <- sqrt(e) # D <- sqrt(E)*(nrow(S) - 1)
+    # u <- t(diag(1 / d) %*% t(v) %*% kpca@xmatrix) # NOTE: This cannot be done! PCA is in kernel space!
 
     names(d) <- paste0("Dim", seq_len(length(d)))
     dimnames(v) <- list(rownames(s), paste0("Dim", seq_len(ncol(v))))
@@ -127,9 +142,10 @@ kernel_ca <- function(obj,
     caobj <- do.call(
         APL::new_cacomp,
         list(
-            "U" = svd_res$U,
-            "V" = svd_res$V,
-            "D" = svd_res$D,
+            "U" = u,
+            "V" = v,
+            "D" = d,
+            "prin_coords_cols" = prin_coords_cols,
             "row_masses" = res$rowm,
             "col_masses" = res$colm,
             "top_rows" = top,
@@ -138,7 +154,7 @@ kernel_ca <- function(obj,
         )
     )
 
-    caobj <- APL::ca_coords(caobj)
+    # caobj <- APL::ca_coords(caobj) # NOTE: Again, we cannot compute this, as we are working in kernel space!
 
     return(caobj)
 }
