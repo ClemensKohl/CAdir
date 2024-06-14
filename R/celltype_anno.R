2d2552e1ea3bb9759b6751d7e45e5c5ffe9abfe0
Clemens Kohl <kohl.clemens@gmail.com>
2024-06-14 16:56:01 +0200

    fixed celltype anno

--- R/celltype_anno.R
+++ R/celltype_anno.R
@@ -15,15 +15,15 @@ setMethod(
         # cell clusters
         ccs <- cell_clusters(obj)
         ccs_nm <- names(ccs)
-        unccs <- levels(ccs))
+        unccs <- levels(ccs)
 
         # gene clusters
         gcs <- gene_clusters(obj)
         gcs_nm <- names(gcs)
-        ungcs <- levels(gcs))
+        ungcs <- levels(gcs)
 
         # Combine clusters
-        allcs <- unique(c(unccs, ungcs)))
+        allcs <- unique(c(unccs, ungcs))
 
         # Convert to character
         ccs <- as.character(ccs)
@@ -49,7 +49,7 @@ setMethod(
 
         # Rename clusters based on GSE.
         for (c in seq_len(length(allcs))) {
-            noanno <- paste0("cluster", allcs[c])
+            noanno <- allcs[c]
 
             if (!allcs[c] %in% cluster_anno$cluster) {
                 ct <- noanno
